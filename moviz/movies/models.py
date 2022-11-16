61b8fb89acaf69e7c26d264eaa768a857a3c5595
J.A <ja1won111@gmail.com>
2022-11-17 00:01:08 +0900

    feat(movies) model complete

--- moviz/movies/models.py
+++ moviz/movies/models.py
@@ -6,3 +6,9 @@ class Movie(models.Model):
     movie_poster_path = models.TextField()
     tmdb_id = models.IntegerField()
     movie_description = models.TextField()
+    
+
+# class Movie_Comment(models.Model):
+    # movie_id = models.ForeignKey(Movie,on_delete=models.CASCADE)
+    # user_id = models.ForeignKey(,on_delete=models.CASCADE)
+    # content = models.TextField()
