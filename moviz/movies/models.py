d4b1b5a2c50771608d549e81ac2085dbfbe731b1
J.A <ja1won111@gmail.com>
2022-11-16 23:58:14 +0900

    feat(movies) Model Movie done

--- moviz/movies/models.py
+++ moviz/movies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    movie_title = models.CharField(max_length=50)
+    movie_poster_path = models.TextField()
+    tmdb_id = models.IntegerField()
+    movie_description = models.TextField()
