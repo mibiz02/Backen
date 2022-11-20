621c9a2604a2bbe725aa51a9a80ca6b55b9ae1f2
J.A <ja1won111@gmail.com>
2022-11-20 15:15:17 +0900

    feat(Accounts) add Accounts

--- moviz/mbti_compabilities/models.py
+++ moviz/mbti_compabilities/models.py
@@ -8,7 +8,7 @@ class MBTI_Type(models.Model):
     # good_matching = models.JSONField(null=True)
     good_matching = models.ManyToManyField('self', symmetrical=False, related_name='good_matched')
     # good_matching = models.JSONField(null=True)
-    bad_match = models.ManyToManyField('self', symmetrical=True, related_name='bad_matched')
+    bad_match = models.ManyToManyField('self', symmetrical=False, related_name='bad_matched')
     # type_users = models.ManyToManyField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # def __str__(self):
     #     return self.id
