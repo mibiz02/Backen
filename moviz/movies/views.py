ce1d63b1672c262895c5efac2b6e59a859c8fa18
J.A <ja1won111@gmail.com>
2022-11-20 16:11:58 +0900

    feat(Movie) set isAuthenticated for movie comment

--- moviz/movies/views.py
+++ moviz/movies/views.py
@@ -1,5 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.shortcuts import get_list_or_404, get_object_or_404
 
@@ -33,6 +34,7 @@ def movie_detail(request, movie_pk):
         return Response(serializer.data)
     
 @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
 def comment_create(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     serializer = CommentSerializer(data=request.data)
@@ -41,6 +43,7 @@ def comment_create(request, movie_pk):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
 @api_view(['GET','DELETE','PUT'])
+# @permission_classes([IsAuthenticated])
 def comment_detail(request, comment_pk):
     comment = get_object_or_404(Movie_Comment, pk=comment_pk)
     
