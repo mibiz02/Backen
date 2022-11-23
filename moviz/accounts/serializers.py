bb13aa958ac685f5fe0308b9cf4e116dda63047c
원정아 <ja1won111@gmail.com>
2022-11-23 13:24:03 +0900

    feat(Accounts) mypage like movie

--- moviz/accounts/serializers.py
+++ moviz/accounts/serializers.py
@@ -5,6 +5,10 @@ from allauth.account.utils import setup_user_email
 
 from dj_rest_auth.registration.serializers import RegisterSerializer
 
+from .models import User
+from movies.models import Movie
+
+
 class CustomRegisterSerializer(RegisterSerializer):
     nickname = serializers.CharField(required=False, max_length=128)
     MBTI_type = serializers.CharField(required=False, max_length=4)
@@ -14,3 +18,30 @@ class CustomRegisterSerializer(RegisterSerializer):
         data_dict['nickname'] = self.validated_data.get('nickname', '')
         data_dict['MBTI_type'] = self.validated_data.get('MBTI_type', '')
         return data_dict
+
+
+class MovieSetSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = ('id',)
+
+class UserSerializer(serializers.ModelSerializer):
+    # like_movies_title = MovieSetSerializer(read_only=True)
+    # like_movies_title = serializers.CharField(source='like_movies.all', read_only=True)
+    # like_movies_title = serializers.ListField(source='like_movies.all', read_only=True)
+    # count_like_movies_set = MovieSerializer(read_only=True, many=True)
+    # count_like_movies
+    # like_movies
+
+
+    class Meta:
+        model = User
+        fields=('username','email','nickname','date_joined','MBTI_type',)
+        # fields=('nickname','like_movies',)
+
+class likeMovieSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = ('title',)
--- moviz/moviz/urls.py
+++ moviz/moviz/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('movies/', include('movies.urls')),
     path('mbti_compabilities/', include('mbti_compabilities.urls')),
     path('accounts/', include('dj_rest_auth.urls')),
-    path('accounts/signup/', include('dj_rest_auth.registration.urls'))
+    path('accounts/signup/', include('dj_rest_auth.registration.urls')),
+    path('mypage/', include('accounts.urls'))
 ]
